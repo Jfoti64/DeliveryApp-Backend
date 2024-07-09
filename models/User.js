60f402ef6f8e0a0ffa48943633eae8dda503d710
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-07-09 17:44:30 -0500

    Add User model

---
+++ models/User.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePicture: { type: String },
+  },
+  { timestamps: true }
+);
+
+// Password hashing middleware
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Password comparison method
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', UserSchema);
+export default User;
